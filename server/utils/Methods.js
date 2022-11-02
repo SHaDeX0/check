e234b19a1af77d138b29d77b067e7943cb87ab47
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-11-02 22:49:43 +0530

    code clean up

--- server/utils/Methods.js
+++ server/utils/Methods.js
@@ -1,23 +1,7 @@
 const UserModel = require('../models/Users.js')
 const bcrypt = require('bcrypt')
-const nodemailer = require('nodemailer')
 
 const saltRounds = 10
-const client = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		type: 'OAuth2',
-		user: 'shadex.dev@gmail.com',
-		pass: 'ltushdrkawnyictp',
-		clientId: '46466161413-ul0lo7ttbmp5748kmjgecjokrdl3l1g1.apps.googleusercontent.com',
-		clientSecret: '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',
-		refreshToken:
-			'1//04aLAksHXXqmdCgYIARAAGAQSNwF-L9Irnm78HYEy9Cvwb3c4wg4FrPTrZvQFcd3FNhmyq87z3bnMhv_wsbgpJFeE--dnHCWMUGw',
-		accessToken:
-			'ya29.a0Aa4xrXNK5_Ld6Hc2ukP5O078UWOuWkLUpBj3XmggT0h1P9myfxb3TuHNTEz6JevSPDDu_J8DXXfAAwM56Z96rRx-2axNe94cbJ0tSd-bGTSY_bUdIbnFVTHK9mLVFdPZRnVsYnjXtrAHRoWC4CbCgUO2pNVoaCgYKATASARMSFQEjDvL9W112uMjdan9W8cSLYWpLwA0163',
-		privateKey: 'AIzaSyCutX9MGVxz6LGqfL-T6ot4RZF8NivZS_c',
-	},
-})
 
 const getAll = (req, res) => {
 	UserModel.find({}, (err, result) => {
@@ -35,21 +19,6 @@ const createUser = async (req, res) => {
 			return
 		}
 		req.session.user = { _id: _id, name: name, password: hash, checklist: [] }
-		client.sendMail(
-			{
-				from: 'shadex.dev@gmail.com',
-				to: _id,
-				subject: 'New Sign In',
-				text: `Hi ${name}! Welcome to Check!`,
-			},
-			(err, response) => {
-				if (err) console.log(err)
-				else {
-					console.log(response)
-					console.log('Mail sent successfully!')
-				}
-			}
-		)
 
 		//creating new user with hashed password
 		const newUser = new UserModel({ _id: _id, name: name, password: hash, checklist: [] })
